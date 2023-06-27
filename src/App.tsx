f9631a2cc22c4ee6ff86e2aa2b05f3d5bb588d9f
hnascx <henriquehusky@gmail.com>
2023-06-26 21:38:06 -0300

    Global styles and eslint config done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+
+import { defaultTheme } from './styles/themes/Default'
+import { GlobalStyle } from './styles/Global'
 
 export function App() {
   return (
-    <>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
-      <Button variant="danger"/>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
       <Button />
-    </>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
